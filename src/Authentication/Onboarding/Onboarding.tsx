173ad9620a2b42ee64217a770fcdfadf710aab20
smshahzaib <smshahzaibshah@gmail.com>
2021-01-03 16:08:23 +0500

    All dependencies were installed

---
+++ src/Authentication/Onboarding/Onboarding.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+import {View} from "react-native";
+
+interface ComponentNameProps {}
+
+const ComponentName=()=>{
+  return <View style={{ flex: 1, backgroundColor: "cyan" }} />;
+}
+export default ComponentName;
